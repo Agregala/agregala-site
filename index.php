3d58923ea735459963cd2a75c9c52895381a7141
Rolando Drouaillet <rdrouaillet@rol-no-roy.local>
2017-09-28 15:44:03 +0200

    change url

--- index.php
+++ index.php
@@ -4,45 +4,35 @@
         <!-- add visualizar -->
         <div class="content-visualizar">
             <?php
+                wp_reset_query();
                 /** Grab any posts for this month (I've chosedn only the last 5 posts) */
-                $args = array(
+                $args1 = array(
                     'posts_per_page'    => -1,
                     'post_type'         => 'post',
                     'post_status'       => 'publish',
                     'order_by'          => 'date'
                 );
-                $month_query = new WP_Query($args);
-
-                if($month_query->have_posts()) :
+                $month_query1 = new WP_Query($args1);
+                $arryaNotin = array();
+                if($month_query1->have_posts()) :
                     /** Loop through each post for this month... */
-                    $contador = 0;
-                    while($month_query->have_posts()) : $month_query->the_post();
-                        $link =  get_permalink(get_the_ID());
+                    $contador1 = 0;
+                    while($month_query1->have_posts()) : $month_query1->the_post();
+            
+                        $thumbID1 = get_post_thumbnail_id( get_the_ID() );
+                        $imgDestacada1 = wp_get_attachment_url( $thumbID1 );
 
-                        $thumbID = get_post_thumbnail_id( get_the_ID() );
-                        $imgDestacada = wp_get_attachment_url( $thumbID );
-
-                        $posttags = get_the_tags();
-                        if ($posttags) {
-                          foreach($posttags as $tag) {
-                            if($tag->name=="Page"){
-                                $varSapn = "faceFondo";
-                            }else{
-                                $varSapn = "rssFondo";
-                            }
-                          }
-                        }
-                        if( $imgDestacada != "" ){
-                            $contador = $contador+1;
+                        if( $imgDestacada1 != "" ){
+                            $contador1 = $contador1+1;
                             $arryaNotin[] = get_the_ID();
-                            if($contador<5){
+                            if($contador1<5){
                                 echo '<div class="col-md-3 col-sm-6">';
-                                    echo '<div class="content-noticias new-style-content" data-link="'.get_post_permalink().'" ">';
+                                    echo '<div class="content-noticias new-style-content" data-link="'.get_permalink(get_the_ID()).'" ">';
                                         /** Output each article for this month */
-                                        echo '<div class="back-img-noti" style="background-image: url('.$imgDestacada.');"></div>';
+                                        echo '<div class="back-img-noti" style="background-image: url('.$imgDestacada1.');"></div>';
                                         //echo '<img style="margin-bottom: 10px; width: 100%;" src="'.$imgDestacada.'" />';
                                         echo '<h3 class="titulo-note-visualizar">'.get_the_title().'</h3>'; 	
-                                        echo '<a class="read-more floats" target="_blank" href="'.get_post_permalink().'">'.get_option('fullby_leermas').'</a>';
+                                        echo '<a class="read-more floats" target="_blank" href="'.get_permalink(get_the_ID()).'">'.get_option('fullby_leermas').'</a>';
                                     echo '</div>';
                                 echo '</div>';
                             }else{
